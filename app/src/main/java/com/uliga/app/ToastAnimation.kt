0d8ce0af5ea750139a01797a953dc4a0015d1da7
ansehoon1999 <ansehoon1999@gmail.com>
2023-09-30 18:25:22 +0900

    refactor: refactoring toast animation

--- app/src/main/java/com/uliga/app/ToastAnimation.kt
+++ app/src/main/java/com/uliga/app/ToastAnimation.kt
@@ -2,15 +2,12 @@ package com.uliga.app
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -19,7 +16,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uliga.app.ui.theme.Primary
@@ -28,8 +28,32 @@ import com.uliga.app.ui.theme.pretendard
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun ToastAnimation(
-    isVisible: Boolean,
-    toastMessage: String,
+    yOffset: Float,
+    toastMessage: String
 ) {
-
+    Box(Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.TopCenter)
+                .offset(
+                    y = yOffset.dp
+                )
+                .shadow(
+                    elevation = 10.dp,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .background(Color.White)
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                color = Primary,
+                fontFamily = pretendard,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+                text = toastMessage
+            )
+        }
+    }
 }
