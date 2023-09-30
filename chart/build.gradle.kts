78eb2392c2104f585de605c53008bcf195290720
ansehoon1999 <ansehoon1999@gmail.com>
2023-09-30 19:40:24 +0900

    feat: implement common package from chart open source

---
+++ chart/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.uliga.chart"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Core.coreKtx)
+    
+    implementation(Coroutines.core)
+    implementation(Coroutines.android)
+
+    implementation(Compose.composeActivity)
+    implementation(Compose.composeUi)
+    implementation(Compose.composeTooling)
+    implementation(Compose.composeFoundation)
+    implementation(Compose.composeHiltNavigation)
+    implementation(Compose.composeMaterial)
+    implementation(Compose.composeMaterial3)
+    implementation(Compose.composeUiPreview)
+    implementation(Compose.composeConstraintLayout)
+
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-process:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation( "androidx.navigation:navigation-compose:2.5.3")
+}
---
+++ chart/src/main/java/com/uliga/chart/common/animation/SimpleChartAnimation.kt
@@ -0,0 +1,7 @@
+package com.uliga.chart.common.animation
+
+import androidx.compose.animation.core.TweenSpec
+
+fun HorizontalChartAnimation() = TweenSpec<Float>(
+    durationMillis = 500
+)
