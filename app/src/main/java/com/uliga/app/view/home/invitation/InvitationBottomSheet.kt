5e2db09124c9dfdef66982f82b896379fc33a670
ansehoon1999 <ansehoon1999@gmail.com>
2023-10-15 16:51:25 +0900

    feat: implement get member logic

--- app/src/main/java/com/uliga/app/view/home/invitation/InvitationBottomSheet.kt
+++ app/src/main/java/com/uliga/app/view/home/invitation/InvitationBottomSheet.kt
@@ -1,6 +1,5 @@
 package com.uliga.app.view.home.invitation
 
-import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
@@ -12,12 +11,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,7 +22,6 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
@@ -36,16 +32,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uliga.app.R
-import com.uliga.app.ui.theme.Grey300
-import com.uliga.app.ui.theme.LightBlue
 import com.uliga.app.ui.theme.Primary
 import com.uliga.app.ui.theme.pretendard
-import com.uliga.app.view.budget.BudgetSettingBottomSheetCompose
-import com.uliga.app.view.main.MainActivity
+import com.uliga.app.view.home.HomeViewModel
+import org.orbitmvi.orbit.compose.collectAsState
 
 
 @RequiresApi(Build.VERSION_CODES.Q)
@@ -53,16 +46,164 @@ import com.uliga.app.view.main.MainActivity
 @Composable
 fun InvitationBottomSheet(
     sheetState: SheetState,
+    viewModel: HomeViewModel,
     onDismissRequest: () -> Unit
 ) {
+
+    val state = viewModel.collectAsState().value
+    val invitation = state.member?.invitations
+
     ModalBottomSheet(
         sheetState = sheetState,
         onDismissRequest = {
             onDismissRequest()
         })
     {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            item {
+                Text(
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 4.dp
+                    ),
+                    text = "새로운 가계부 초대",
+                    color = Color.Black,
+                    fontFamily = pretendard,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
 
-        InvitationBottomSheetCompose()
+            items(invitation?.size ?: 0) { idx ->
+                Column(
+                    modifier = Modifier
+                        .clickable {
+
+                        }
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Image(
+                            modifier = Modifier
+                                .size(52.dp),
+                            painter = painterResource(
+                                id = R.drawable.ic_invitation_logo
+                            ),
+                            contentDescription = null
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Column(
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "${invitation?.get(idx)?.memberName ?: ""}님의 초대",
+                                color = Color(0xFF9B9B9B),
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 14.sp,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+
+                            Text(
+                                text = "${invitation?.get(idx)?.accountBookName}",
+                                color = Color(0xFF464656),
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        Button(
+                            modifier = Modifier
+                                .wrapContentSize(),
+                            contentPadding = PaddingValues(
+                                vertical = 12.dp,
+                                horizontal = 12.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFE9EEFE),
+                            ),
+                            shape = RoundedCornerShape(7.dp),
+                            onClick = {
+
+                            }) {
+                            Text(
+                                color = Primary,
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                text = "거절"
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Button(
+                            modifier = Modifier
+                                .wrapContentSize(),
+                            contentPadding = PaddingValues(
+                                vertical = 12.dp,
+                                horizontal = 12.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Primary,
+                            ),
+                            shape = RoundedCornerShape(7.dp),
+                            onClick = {
+
+                            }) {
+                            Text(
+                                color = Color.White,
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                text = "수락"
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                    }
+
+                    Spacer(
+                        modifier = Modifier.height(20.dp)
+                    )
+
+                    androidx.compose.material.Divider(
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp
+                            ),
+                        color = Color(0xFFE3E3E3),
+                        thickness = 1.dp
+                    )
+
+                }
+
+            }
+        }
+
+//        InvitationBottomSheetCompose()
     }
 }
 
@@ -211,7 +352,6 @@ fun InvitationBottomSheetCompose() {
 
             }
         }
-
 
 
     }
