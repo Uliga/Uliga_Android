6f0d73fde86a2d3a99bb484282b8e44c10654f5e
ansehoon1999 <ansehoon1999@gmail.com>
2023-10-21 14:23:42 +0900

    feat: get accountBook asset and show income/record to view

--- app/src/main/java/com/uliga/app/view/finance/FinanceUiState.kt
+++ app/src/main/java/com/uliga/app/view/finance/FinanceUiState.kt
@@ -1,6 +1,8 @@
 package com.uliga.app.view.finance
 
 import androidx.compose.runtime.Immutable
+import com.uliga.domain.model.accountBook.asset.month.AccountBookAssetMonth
+import com.uliga.domain.model.accountBook.transaction.AccountBookTransactionResponse
 
 @Immutable
 data class FinanceUiState(
@@ -8,13 +10,17 @@ data class FinanceUiState(
     val exception: Exception? = null,
     val id: Long? = null,
     val currentAccountInfo: Pair<String, Long>? = null,
+    val currentAccountBookAsset: AccountBookAssetMonth? = null,
+    val currentAccountBookTransaction: AccountBookTransactionResponse? = null
 ) {
     companion object {
         fun empty() = FinanceUiState(
             isLoading = false,
             exception = null,
             id = null,
-            currentAccountInfo = null
+            currentAccountInfo = null,
+            currentAccountBookAsset = null,
+            currentAccountBookTransaction = null
         )
     }
 }
