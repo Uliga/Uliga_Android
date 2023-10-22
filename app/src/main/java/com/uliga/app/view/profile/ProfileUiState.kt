568c00d8656835efe8a48b8291617ef00fee8387
ansehoon1999 <ansehoon1999@gmail.com>
2023-10-22 17:00:34 +0900

    feat: implement profile baseinfo initial function

--- app/src/main/java/com/uliga/app/view/profile/ProfileUiState.kt
+++ app/src/main/java/com/uliga/app/view/profile/ProfileUiState.kt
@@ -1,16 +1,23 @@
 package com.uliga.app.view.profile
 
+import com.uliga.domain.model.member.Member
 import javax.annotation.concurrent.Immutable
 
 @Immutable
 data class ProfileUiState(
     val isLoading: Boolean = false,
     val exception: Exception? = null,
+    val id: Long? = null,
+    val currentAccountInfo: Pair<String, Long>? = null,
+    val member: Member? = null
 ) {
     companion object {
         fun empty() = ProfileUiState(
             isLoading = false,
-            exception = null
+            exception = null,
+            id = null,
+            currentAccountInfo = null,
+            member = null
         )
     }
 }
--- app/src/main/java/com/uliga/app/view/profile/ProfileViewModel.kt
+++ app/src/main/java/com/uliga/app/view/profile/ProfileViewModel.kt
@@ -1,8 +1,11 @@
 package com.uliga.app.view.profile
 
 import androidx.lifecycle.ViewModel
+import com.uliga.domain.model.member.Member
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
@@ -11,5 +14,18 @@ class ProfileViewModel @Inject constructor(
 ) : ContainerHost<ProfileUiState, ProfileSideEffect>, ViewModel() {
     override val container = container<ProfileUiState, ProfileSideEffect>(ProfileUiState.empty())
 
+    init {
 
+    }
+
+    fun initializeBaseInfo(id: Long?, currentAccountInfo: Pair<String, Long>?, member: Member?) =
+        intent {
+            reduce {
+                state.copy(
+                    id = id,
+                    currentAccountInfo = currentAccountInfo,
+                    member = member
+                )
+            }
+        }
 }
