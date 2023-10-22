75a041ecdbe03ba83a6d7372796fd89fe79e7dc1
ansehoon1999 <ansehoon1999@gmail.com>
2023-10-22 17:42:00 +0900

    feat: implement delete data function when logout/delete

--- domain/src/main/java/com/uliga/domain/usecase/member/DeleteMemberUseCase.kt
+++ domain/src/main/java/com/uliga/domain/usecase/member/DeleteMemberUseCase.kt
@@ -1,14 +1,26 @@
 package com.uliga.domain.usecase.member
 
+import com.uliga.domain.repository.AccountBookRepository
 import com.uliga.domain.repository.MemberRepository
+import com.uliga.domain.repository.UserAuthRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DeleteMemberUseCase @Inject constructor(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val userAuthRepository: UserAuthRepository,
+    private val accountBookRepository: AccountBookRepository
 ) {
     suspend operator fun invoke(): Result<String> {
-        return memberRepository.deleteMember()
+
+        val deleteResult = memberRepository.deleteMember()
+
+        userAuthRepository.updateToken("")
+        userAuthRepository.updateId(0L)
+        accountBookRepository.updateCurrentAccountBookId(0L)
+        accountBookRepository.updateCurrentAccountBookName("")
+
+        return deleteResult
     }
 }
