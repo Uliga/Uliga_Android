088b1c9a3a65fdc01f99efde7e3f90c2762257b4
ansehoon1999 <ansehoon1999@gmail.com>
2023-11-11 15:05:10 +0900

    refactor: refactoring signup screen using UI Component

--- app/src/main/java/com/uliga/app/view/auth/signup/SocialSignUpScreen.kt
+++ app/src/main/java/com/uliga/app/view/auth/signup/SocialSignUpScreen.kt
@@ -9,30 +9,23 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,29 +33,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.uliga.app.R
 import com.uliga.app.ToastAnimation
-import com.uliga.app.ui.theme.Black
 import com.uliga.app.ui.theme.CustomGrey100
 import com.uliga.app.ui.theme.CustomGrey200
-import com.uliga.app.ui.theme.CustomGrey600
-import com.uliga.app.ui.theme.Grey300
-import com.uliga.app.ui.theme.Grey400
+import com.uliga.app.ui.theme.Grey500
+import com.uliga.app.ui.theme.Grey700
 import com.uliga.app.ui.theme.Primary
-import com.uliga.app.ui.theme.pretendard
+import com.uliga.app.ui.theme.UligaTheme
+import com.uliga.app.ui.theme.White
 import com.uliga.app.view.CircularProgress
-import com.uliga.app.view.accountBook.generation.AccountBookGenerationActivity
+import com.uliga.app.view.accountBook.selection.AccountBookSelectionActivity
 import com.uliga.app.view.auth.AuthSideEffect
 import com.uliga.app.view.auth.AuthViewModel
+import com.uliga.app.view.component.AgreeCheckbox
+import com.uliga.app.view.component.BasicTextField
+import com.uliga.app.view.component.CheckButton
+import com.uliga.app.view.component.PositiveButton
+import com.uliga.app.view.component.VerticalSpacer
 import com.uliga.app.view.main.MainActivity
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
@@ -125,14 +119,16 @@ fun SocialSignupScreen(
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(White),
         verticalArrangement = Arrangement.spacedBy(20.dp),
     ) {
 
         item {
 
             CenterAlignedTopAppBar(
-                modifier = Modifier.background(Color.White),
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = White
+                ),
                 title = {
                     Row {
                         Image(
@@ -141,7 +137,7 @@ fun SocialSignupScreen(
                             painter = painterResource(
                                 id = R.drawable.uliga_logo
                             ),
-                            contentDescription = "uliga logo"
+                            contentDescription = null
                         )
 
                         Text(
@@ -150,9 +146,7 @@ fun SocialSignupScreen(
                             ),
                             text = "우리가",
                             color = Primary,
-                            fontFamily = pretendard,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 24.sp
+                            style = UligaTheme.typography.title1
                         )
                     }
                 },
@@ -169,32 +163,78 @@ fun SocialSignupScreen(
                     )
             ) {
                 Text(
-
                     text = "회원가입",
-                    color = Black,
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp
+                    color = Grey700,
+                    style = UligaTheme.typography.title1
                 )
 
-                Spacer(
-                    modifier = Modifier
-                        .height(8.dp)
-                )
+                VerticalSpacer(height = 8.dp)
 
                 Text(
                     text = "만나서 반가워요! 당신의 건강한 소비 생활을 응원합니다\uD83D\uDE4B\u200D♀️",
-                    color = CustomGrey600,
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 14.sp,
+                    color = Grey500,
+                    style = UligaTheme.typography.body12,
                     maxLines = 2
                 )
 
-                Spacer(
+                VerticalSpacer(height = 20.dp)
+            }
+        }
+
+        item {
+            Column(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .padding(
+                        horizontal = 16.dp
+                    )
+            ) {
+
+                Text(
+                    textAlign = TextAlign.Start,
                     modifier = Modifier
-                        .height(20.dp)
+                        .fillMaxWidth(),
+                    text = "닉네임",
+                    color = Grey700,
+                    style = UligaTheme.typography.title3
                 )
+
+                VerticalSpacer(height = 8.dp)
+
+                Row {
+                    Box(
+                        modifier = Modifier.weight(4f)
+                    ) {
+                        BasicTextField(
+                            value = nickNameTextState.value,
+                            onValueChange = {
+                                nickNameTextState.value = it
+                            },
+                            keyboardType = KeyboardType.Text
+                        )
+                    }
+
+                    CheckButton(
+                        enabled = state.isNickNameExisted == null || state.isNickNameExisted,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .padding(horizontal = 4.dp),
+                        contentPadding = PaddingValues(
+                            vertical = 4.dp
+                        ),
+                        textColor = if (state.isNickNameExisted == null || state.isNickNameExisted) Primary else White,
+                        backgroundColor = ButtonDefaults.buttonColors(
+                            backgroundColor =
+                            if (state.isNickNameExisted == null || state.isNickNameExisted) CustomGrey200
+                            else CustomGrey100,
+                        ),
+                        text = "확인",
+                        onClick = {
+                            viewModel.getIsNickNameExisted(nickNameTextState.value)
+                        }
+                    )
+                }
             }
         }
 
@@ -211,32 +251,18 @@ fun SocialSignupScreen(
                     modifier = Modifier
                         .fillMaxWidth(),
                     text = "이메일 주소",
+                    color = Grey700,
+                    style = UligaTheme.typography.title3
                 )
 
-                Spacer(
-                    modifier = Modifier
-                        .height(8.dp)
-                )
+                VerticalSpacer(height = 8.dp)
 
-
-
-                TextField(
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Grey400, shape = RoundedCornerShape(10.dp))
-                        .fillMaxWidth(),
+                BasicTextField(
                     value = emailAddressTextState.value,
                     onValueChange = {
                         emailAddressTextState.value = it
                     },
-                    textStyle = TextStyle(
-                        color = Color.Black, fontSize = 20.sp
-                    ),
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                    ),
-                    singleLine = true,
+                    keyboardType = KeyboardType.Text
                 )
             }
         }
@@ -255,101 +281,19 @@ fun SocialSignupScreen(
                     modifier = Modifier
                         .fillMaxWidth(),
                     text = "이름",
+                    color = Grey700,
+                    style = UligaTheme.typography.title3
                 )
 
-                Spacer(
-                    modifier = Modifier
-                        .height(8.dp)
-                )
+                VerticalSpacer(height = 8.dp)
 
-                TextField(
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Grey400, shape = RoundedCornerShape(10.dp))
-                        .fillMaxWidth(),
+                BasicTextField(
                     value = nameTextState.value,
                     onValueChange = {
                         nameTextState.value = it
                     },
-                    textStyle = TextStyle(
-                        color = Color.Black, fontSize = 20.sp
-                    ),
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                    ),
-                    singleLine = true,
+                    keyboardType = KeyboardType.Text
                 )
-            }
-        }
-
-        item {
-            Column(
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .padding(
-                        horizontal = 16.dp
-                    )
-            ) {
-                Text(
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    text = "닉네임",
-                )
-
-                Spacer(
-                    modifier = Modifier
-                        .height(8.dp)
-                )
-
-
-
-                TextField(
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Grey400, shape = RoundedCornerShape(10.dp))
-                        .fillMaxWidth(),
-                    value = nickNameTextState.value,
-                    onValueChange = {
-                        nickNameTextState.value = it
-                    },
-                    textStyle = TextStyle(
-                        color = Color.Black, fontSize = 20.sp
-                    ),
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                    ),
-                    singleLine = true,
-                )
-
-                Button(
-                    enabled = state.isNickNameExisted == null || state.isNickNameExisted,
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .padding(horizontal = 4.dp),
-                    contentPadding = PaddingValues(
-                        vertical = 4.dp
-                    ),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor =
-                        if (state.isNickNameExisted == null || state.isNickNameExisted) CustomGrey200
-                        else CustomGrey100,
-                    ),
-                    shape = RoundedCornerShape(10.dp),
-                    onClick = {
-                        viewModel.getIsNickNameExisted(nickNameTextState.value)
-                    }) {
-                    Text(
-                        color = if (state.isNickNameExisted == null || state.isNickNameExisted) Primary
-                        else Grey300,
-                        fontFamily = pretendard,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 12.sp,
-                        text = "확인"
-                    )
-                }
             }
         }
 
@@ -358,63 +302,43 @@ fun SocialSignupScreen(
                 modifier = Modifier.padding(horizontal = 8.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Checkbox(
+
+                AgreeCheckbox(
+                    text = "우리가 개인정보 수집 및 이용 동의 (필수)",
                     checked = privacyCheckBoxState.value,
-                    onCheckedChange = {
+                    onCheckChange = {
                         privacyCheckBoxState.value = !privacyCheckBoxState.value
                         viewModel.fetchIsPrivacyChecked(privacyCheckBoxState.value)
                     }
                 )
-
-                Text(
-                    color = CustomGrey600,
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 12.sp,
-                    text = "우리가 개인정보 수집 및 이용 동의 (필수)",
-                )
-
             }
         }
 
         item {
 
-            Button(
+            PositiveButton(
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
                     .padding(horizontal = 16.dp),
+                text = "계정 만들기",
                 contentPadding = PaddingValues(
                     vertical = 16.dp
                 ),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Primary,
-                ),
-                shape = RoundedCornerShape(10.dp),
                 onClick = {
-                  viewModel.postSocialLogin(
-                      email = emailAddressTextState.value,
-                      userName = nameTextState.value,
-                      nickName = nickNameTextState.value,
-                      privacyCheckBoxState = privacyCheckBoxState.value
-                  )
-
-                }) {
-                Text(
-                    color = Color.White,
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.Light,
-                    fontSize = 16.sp,
-                    text = "계정 만들기"
-                )
-            }
-
-
+                    viewModel.postSocialLogin(
+                        email = emailAddressTextState.value,
+                        userName = nameTextState.value,
+                        nickName = nickNameTextState.value,
+                        privacyCheckBoxState = privacyCheckBoxState.value
+                    )
+                }
+            )
         }
 
     }
 
-    if(state.isLoading) {
+    if (state.isLoading) {
         CircularProgress()
     }
 
@@ -445,9 +369,10 @@ private fun handleSideEffect(
         }
 
         is AuthSideEffect.NavigateToAccountBookGenerationActivity -> {
-            val intent = Intent(context, AccountBookGenerationActivity::class.java)
+            val intent = Intent(context, AccountBookSelectionActivity::class.java)
             context.startActivity(intent)
         }
+
         else -> {}
     }
 }
