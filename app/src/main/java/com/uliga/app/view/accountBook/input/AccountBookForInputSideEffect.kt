df25edc65d660f0e4e031d64bf10d031d52e8e08
ansehoon1999 <ansehoon1999@gmail.com>
2023-11-11 14:52:38 +0900

    refactor: refactoring schedule selection bottom sheet -> activity

--- app/src/main/java/com/uliga/app/view/accountBook/input/AccountBookForInputSideEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.uliga.app.view.accountBook.input
-
-sealed class AccountBookForInputSideEffect {
-
-    object Finish : AccountBookForInputSideEffect()
-
-    data class ToastMessage(val toastMessage: String) : AccountBookForInputSideEffect()
-
-}
