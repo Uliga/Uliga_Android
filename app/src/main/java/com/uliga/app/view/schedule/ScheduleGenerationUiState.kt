880735d4cc1062c599edd7402cdc884d2862b689
ansehoon1999 <ansehoon1999@gmail.com>
2023-11-11 15:00:05 +0900

    refactor: refactoring schedule generation bottom sheet -> activity

---
+++ app/src/main/java/com/uliga/app/view/schedule/ScheduleGenerationUiState.kt
@@ -0,0 +1,25 @@
+package com.uliga.app.view.schedule
+
+import androidx.compose.runtime.Immutable
+import com.uliga.domain.model.financeSchedule.common.FinanceSchedule
+import com.uliga.domain.model.financeSchedule.common.FinanceSchedules
+import com.uliga.domain.model.member.Member
+
+@Immutable
+
+data class ScheduleGenerationUiState(
+    val isLoading: Boolean = false,
+    val id: Long? = null,
+    val currentAccountInfo: Pair<String, Long>? = null,
+    val financeSchedules: FinanceSchedules? = null,
+    val member: Member? = null
+) {
+    companion object {
+        fun empty() = ScheduleGenerationUiState(
+            isLoading = false,
+            id = null,
+            currentAccountInfo = null,
+            financeSchedules = null
+        )
+    }
+}
