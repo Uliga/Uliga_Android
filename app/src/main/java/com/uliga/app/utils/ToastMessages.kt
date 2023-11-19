d3ef84f0584ed112475ab24797dc49725867167b
ansehoon1999 <ansehoon1999@gmail.com>
2023-11-19 16:09:37 +0900

    chore: make toast message constant strings

---
+++ app/src/main/java/com/uliga/app/utils/ToastMessages.kt
@@ -0,0 +1,26 @@
+package com.uliga.app.utils
+
+object ToastMessages {
+
+    const val MEMBER_INFO_GET_FAILURE = "사용자 정보를 가져오는데 실패했습니다."
+
+    const val ACCOUNT_BOOK_INFO_GET_FAILURE = "가계부 정보를 가져오는데 실패했습니다."
+
+    const val ACCOUNT_BOOK_JOIN_SUCCESS = "가계부에 합류하였습니다!"
+
+    const val FINANCE_SCHEDULE_DELETE_SUCCESS = "금융 일정을 삭제하는데 성공했습니다."
+
+    const val FINANCE_SCHEDULE_DELETE_FAILURE = "금융 일정을 삭제하는데 실패했습니다."
+
+    const val BUDGET_INPUT_RETRY = "예산을 입력해주세요."
+
+    const val BUDGET_INPUT_CHECK_AGAIN = "예산을 다시 확인해주세요."
+
+    const val BUDGET_INPUT_MODIFY_SUCCESS = "예산을 수정하는데 성공했습니다."
+
+    const val BUDGET_REGISTER_SUCCESS = "예산을 등록하는데 성공했습니다."
+
+    const val TRANSACTION_DELETE_SUCCESS = "거래 내역을 삭제하는데 성공했습니다!"
+
+}
+
