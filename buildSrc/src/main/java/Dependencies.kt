df4156ae3a2d3d24ff75664a4f28fc90e0e14bc0
ansehoon1999 <ansehoon1999@gmail.com>
2023-12-03 15:35:35 +0900

    chore: fix version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@ object App {
     const val compileSdk = 33
     const val minSdk = 27
     const val targetSdk = 33
-    const val versionCode = 7
+    const val versionCode = 1
     const val versionName = "1.0.0"
     const val buildTools = "30.0.3"
 }
